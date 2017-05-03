6e16943817fc96a20d4452415e0fb01957d131e8
Donald Moore <donald@maximumdev.biz>
2017-05-03 02:50:03 -0500

    Added live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
 	concat = require('gulp-concat');
 
 // gulp.task('log', function() {
@@ -30,6 +31,7 @@ gulp.task('js', function() {
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+		.pipe(connect.reload())
 });
 
 gulp.task('compass', function() {
@@ -41,12 +43,20 @@ gulp.task('compass', function() {
 		})
 			.on('error', gutil.log))
 		.pipe(gulp.dest('builds/development/css'))
+		.pipe(connect.reload())
 });
-
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
 
 gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
 });
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'builds/development/',
+		livereload: true
+	});
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
